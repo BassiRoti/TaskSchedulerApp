4335de94bd05fab9aa5c0506d367276168bbacb7
Abbas Rizvi <abbasriz1234567890@gmail.com>
2025-05-01 19:17:57 +0500

    done

--- app/src/main/java/com/example/taskschedulerapp/ProfileTabFragment.java
+++ app/src/main/java/com/example/taskschedulerapp/ProfileTabFragment.java
@@ -1,12 +1,21 @@
 package com.example.taskschedulerapp;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,50 +24,118 @@ import android.view.ViewGroup;
  */
 public class ProfileTabFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+EditText name, email;
+Switch sw;
+View v;
+LinearLayout ly;
+Button editbtn;
+boolean isEditMode=false;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileTabFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileTabFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileTabFragment newInstance(String param1, String param2) {
-        ProfileTabFragment fragment = new ProfileTabFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+SharedPreferences sp;
+private void init(){
+    name=v.findViewById(R.id.displayName);
+    email=v.findViewById(R.id.displayEmail);
+    sw=v.findViewById(R.id.themeToggle);
+    editbtn=v.findViewById(R.id.editButton);
+    sp= requireContext().getSharedPreferences("pp",MODE_PRIVATE);
+    ly=v.findViewById(R.id.ly);
+}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        v= inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init();
+
+
+        String sname = sp.getString("pname", "Default Name");
+        String semail = sp.getString("pemail", "Default Email");
+        boolean isDarkMode = sp.getBoolean("ptheme", false);
+
+        name.setText(sname);
+        email.setText(semail);
+        sw.setChecked(isDarkMode);
+        name.setEnabled(false);
+        email.setEnabled(false);
+        sw.setChecked(isDarkMode);
+
+        if (isDarkMode) {
+            ly.setBackgroundColor(0xFF121212);
+            name.setBackgroundColor(0xFF333333);
+            email.setBackgroundColor(0xFF333333);
+            name.setTextColor(0xFFFFFFFF);
+            email.setTextColor(0xFFFFFFFF);
+            sw.setTextColor(0xFFFFFFFF);
+            editbtn.setBackgroundColor(0xFF6A1B9A);
+        } else {
+            ly.setBackgroundColor(0xFFFFFFFF);
+            name.setBackgroundColor(0xFFFFFFFF);
+            email.setBackgroundColor(0xFFFFFFFF);
+            name.setTextColor(0xFF000000);
+            email.setTextColor(0xFF000000);
+            sw.setTextColor(0xFF000000);
+            editbtn.setBackgroundColor(0xFF6A1B9A);
+        }
+
+        editbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isEditMode){
+                    name.setEnabled(true);
+                    email.setEnabled(true);
+                    editbtn.setText("Save");
+                    isEditMode = true;
+                }
+                else{
+                    String newName = name.getText().toString();
+                    String newEmail = email.getText().toString();
+
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putString("pname", newName);
+                    editor.putString("pemail", newEmail);
+                    editor.apply();
+
+                    name.setEnabled(false);
+                    email.setEnabled(false);
+                    editbtn.setText("Edit");
+                    isEditMode = false;
+                }
+            }
+        });
+
+        sw.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putBoolean("ptheme", isChecked);
+            editor.apply();
+
+            if (isChecked) {
+                // Apply dark theme
+                ly.setBackgroundColor(0xFF121212);
+                name.setBackgroundColor(0xFF333333);
+                email.setBackgroundColor(0xFF333333);
+                name.setTextColor(0xFFFFFFFF);
+                email.setTextColor(0xFFFFFFFF);
+                sw.setTextColor(0xFFFFFFFF);
+                editbtn.setBackgroundColor(0xFF6A1B9A);
+            } else {
+                // Apply light theme
+                ly.setBackgroundColor(0xFFFFFFFF);
+                name.setBackgroundColor(0xFFFFFFFF);
+                email.setBackgroundColor(0xFFFFFFFF);
+                name.setTextColor(0xFF000000);
+                email.setTextColor(0xFF000000);
+                sw.setTextColor(0xFF000000);
+                editbtn.setBackgroundColor(0xFF6A1B9A);
+            }
+        });
+
+
     }
 }
