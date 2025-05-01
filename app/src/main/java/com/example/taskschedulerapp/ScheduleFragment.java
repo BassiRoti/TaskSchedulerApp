784d3a600eb00479a273d49eccb244de2841ad26
Abbas Rizvi <abbasriz1234567890@gmail.com>
2025-05-01 15:56:12 +0500

    Create new task implemented

--- app/src/main/java/com/example/taskschedulerapp/ScheduleFragment.java
+++ app/src/main/java/com/example/taskschedulerapp/ScheduleFragment.java
@@ -2,11 +2,16 @@ package com.example.taskschedulerapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,50 +20,47 @@ import android.view.ViewGroup;
  */
 public class ScheduleFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    EditText etname,etdesc,etdate,etttime,etstatus;
+    Button btn;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    View v;
 
-    public ScheduleFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScheduleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ScheduleFragment newInstance(String param1, String param2) {
-        ScheduleFragment fragment = new ScheduleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    private void init(){
+        etname=v.findViewById(R.id.taskTitle);
+        etdesc=v.findViewById(R.id.taskDesc);
+        etdate=v.findViewById(R.id.taskDate);
+        etttime=v.findViewById(R.id.taskTime);
+        etstatus=v.findViewById(R.id.taskStatus);
+        btn=v.findViewById(R.id.saveButton);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        v= inflater.inflate(R.layout.fragment_schedule, container, false);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String recname=etname.getText().toString().trim();
+                String recdesc=etdesc.getText().toString().trim();
+                String recdate=etdate.getText().toString().trim();
+                String rectime=etttime.getText().toString().trim();
+                String recstatus=etstatus.getText().toString().trim();
+
+                ScheduleDB db=new ScheduleDB(requireContext());
+                db.open();
+                db.addschedule(recname,recdesc,recdate,rectime,recstatus);
+                Toast.makeText(requireContext(), "Saved successfully", Toast.LENGTH_SHORT).show();
+                db.close();
+            }
+        });
     }
 }
