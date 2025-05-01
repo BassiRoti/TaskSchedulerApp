14cb7c022f101d19f5b44e512262ae9ec00d2d92
Abbas Rizvi <abbasriz1234567890@gmail.com>
2025-05-01 15:24:20 +0500

    implemented tasks db and past activities

--- app/src/main/java/com/example/taskschedulerapp/PastTabFragment.java
+++ app/src/main/java/com/example/taskschedulerapp/PastTabFragment.java
@@ -2,11 +2,14 @@ package com.example.taskschedulerapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,50 +18,29 @@ import android.view.ViewGroup;
  */
 public class PastTabFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public PastTabFragment() {
-        // Required empty public constructor
+    TextView pastactivities;
+    View v;
+    private void init(){
+       pastactivities=v.findViewById(R.id.pastactivities);
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PastTabFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PastTabFragment newInstance(String param1, String param2) {
-        PastTabFragment fragment = new PastTabFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_past_tab, container, false);
+         v=inflater.inflate(R.layout.fragment_past_tab, container, false);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init();
+        ScheduleDB db=new ScheduleDB(requireContext());
+        db.open();
+        pastactivities.setText(db.pasttasks());
+        db.close();
     }
 }
---
+++ app/src/main/java/com/example/taskschedulerapp/ScheduleDB.java
@@ -0,0 +1,143 @@
+package com.example.taskschedulerapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.DatePicker;
+
+import androidx.annotation.Nullable;
+
+import java.text.DateFormat;
+
+public class ScheduleDB  {
+
+    private final String DATABASE_NAME="ScheduleDB";
+    private final String DATABASE_TABLE="ScheduleTable";
+    private final int DATABASE_VERSION=1;
+    public static String ROW_ID="_id";
+    public static String ROW_TITLE="_title";
+    public static String ROW_DESC="_desc";
+    public static String ROW_DATE="_date";
+    public static String ROW_TIME="_time";
+    public static String ROW_STATUS="_status";
+
+    public Context context;
+
+    public ScheduleDB(Context c){
+        context=c;
+    }
+
+    public class ScheduleHelper extends SQLiteOpenHelper{
+
+        public ScheduleHelper(@Nullable Context context) {
+            super(context, DATABASE_NAME,null, DATABASE_VERSION,null);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String query="CREATE TABLE "+DATABASE_TABLE+" ("+
+                    ROW_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+ROW_TITLE+" TEXT NOT NULL, "+ROW_DESC+" TEXT NOT NULL, "
+                    +ROW_DATE+" TEXT NOT NULL, "+ROW_TIME+" TEXT NOT NULL, " +ROW_STATUS+" INTEGER NOT NULL);";
+
+            db.execSQL(query);
+            insertSampleData(db);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS "+DATABASE_TABLE);
+            onCreate(db);
+        }
+    }
+
+    ScheduleHelper scheduleHelper;
+    SQLiteDatabase db;
+
+    public void open(){
+        scheduleHelper=new ScheduleHelper(context);
+        db=scheduleHelper.getWritableDatabase();
+    }
+
+    public void close(){
+        scheduleHelper.close();
+    }
+
+    public long addschedule(String title, String desc, String date, String time, String status ){
+        ContentValues cv=new ContentValues();
+        cv.put(ROW_TITLE,title);
+        cv.put(ROW_DESC,desc);
+        cv.put(ROW_DATE,date);
+        cv.put(ROW_TIME,time);
+        cv.put(ROW_STATUS,status);
+        return db.insert(DATABASE_TABLE,null,cv);
+    }
+
+    public String pasttasks(){
+        String[] columns = new String[]{ROW_ID, ROW_TITLE, ROW_DESC, ROW_DATE, ROW_TIME,ROW_STATUS};
+        Cursor c = db.query(DATABASE_TABLE, columns, null, null, null, null, null);
+        if (c != null && c.moveToFirst()) {
+            StringBuilder result = new StringBuilder();
+
+            int indexTitle = c.getColumnIndex(ROW_TITLE);
+            int indexDesc = c.getColumnIndex(ROW_DESC);
+            int indexDate = c.getColumnIndex(ROW_DATE);
+            int indexTime = c.getColumnIndex(ROW_TIME);
+            int indexStatus = c.getColumnIndex(ROW_STATUS);
+
+            do {
+                String title = c.getString(indexTitle);
+                String desc = c.getString(indexDesc);
+                String date = c.getString(indexDate);
+                String time = c.getString(indexTime);
+                String status = c.getString(indexStatus);
+
+
+                result.append("\n")
+                        .append("Title: ").append(title).append("\n")
+                        .append("Description: ").append(desc).append("\n")
+                        .append("Date: ").append(date).append("\n")
+                        .append("Time: ").append(time).append("\n")
+                        .append("Status: ").append(status)
+                        .append("\n----------------------------\n");
+            } while (c.moveToNext());
+
+            c.close();
+            return result.toString();
+        }
+
+        return "No data found";
+    }
+    public void insertSampleData(SQLiteDatabase db) {
+        ContentValues values = new ContentValues();
+
+        values.put(ROW_ID, 1);
+        values.put(ROW_TITLE, "Task 1");
+        values.put(ROW_DESC, "Description 1");
+        values.put(ROW_DATE, "2025-04-28");
+        values.put(ROW_TIME, "10:00");
+        values.put(ROW_STATUS, "Pending");
+        db.insert(DATABASE_TABLE, null, values);
+
+        values.put(ROW_ID, 2);
+        values.put(ROW_TITLE, "Task 2");
+        values.put(ROW_DESC, "Description 2");
+        values.put(ROW_DATE, "2025-05-01");
+        values.put(ROW_TIME, "09:00");
+        values.put(ROW_STATUS, "Completed");
+        db.insert(DATABASE_TABLE, null, values);
+
+        values.put(ROW_ID, 3);
+        values.put(ROW_TITLE, "Task 3");
+        values.put(ROW_DESC, "Description 3");
+        values.put(ROW_DATE, "2025-04-30");
+        values.put(ROW_TIME, "15:30");
+        values.put(ROW_STATUS, "In Progress");
+        db.insert(DATABASE_TABLE, null, values);
+    }
+
+
+
+
+}
